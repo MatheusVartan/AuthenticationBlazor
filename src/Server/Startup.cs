636846e4602849cf8a376fd8cf655345c70e764a
reshiteguimeti <matheusvartan@gmail.com>
2019-12-31 20:01:55 -0400

    Inserido o Identity

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -1,18 +1,52 @@
+using ListGeneratorBlazor.Server.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Linq;
+using System.Text;
 
 namespace ListGeneratorBlazor.Server
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ListGeneratorContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ListGeneratorBlazorConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ListGeneratorContext>()
+                .AddDefaultTokenProviders();
+
+           services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+           options.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = false,
+               ValidateAudience = false,
+               ValidateLifetime = true,
+               ValidateIssuerSigningKey = true,
+               IssuerSigningKey = new SymmetricSecurityKey(
+               Encoding.UTF8.GetBytes(Configuration["jwt:key"])),
+               ClockSkew = TimeSpan.Zero
+           });
+
             services.AddMvc();
             services.AddResponseCompression(opts =>
             {
@@ -36,6 +70,9 @@ namespace ListGeneratorBlazor.Server
             app.UseClientSideBlazorFiles<Client.Startup>();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
